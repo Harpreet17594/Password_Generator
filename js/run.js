9bff57427a78da2b12c04c41d263f9f09ebf6140
harpreet kaur <harpreetvirdi17594@gmail.com>
2023-07-22 00:32:59 +0100

    Updated run.js file added some code to refine the results of code

--- js/run.js
+++ js/run.js
@@ -29,6 +29,39 @@ const types = {
     }
   ];
 
+  //new array no uppercase letters
+  
+  const noUppercase = [
+
+    // Function to randomly select an lowercase letters
+    function lowerCasedCharacters() {
+      return types.lowerCasedCharacters[Math.floor(Math.random() * types.lowerCasedCharacters.length)];
+    },
+
+    // Function to randomly select a numericCharacters
+    function numericCharacters() {
+      return types.numericCharacters[Math.floor(Math.random() * types.numericCharacters.length)];
+    },
+
+    // Function to randomly select a specialCharacters
+    function specialCharacters() {
+      return types.specialCharacters[Math.floor(Math.random() * types.specialCharacters.length)];
+    }
+  ];
+
+  //new array no lowercase letters
+  const noLowercase = [
+
+    // Function to randomly select a numericCharacters
+    function numericCharacters() {
+      return types.numericCharacters[Math.floor(Math.random() * types.numericCharacters.length)];
+    },
+
+    // Function to randomly select a specialCharacters
+    function specialCharacters() {
+      return types.specialCharacters[Math.floor(Math.random() * types.specialCharacters.length)];
+    }
+  ];
 
 //function call on button to generate button click
 function generatePassword() 
@@ -36,20 +69,52 @@ function generatePassword()
   let storeValue = ""; // the generated password variable
   
       let userInput = prompt("Please enter a password length between 8 and 128 characters");
-        console.log(typeof(userInput));
+        // console.log(typeof(userInput));
         
   
-        if (userInput>=8 && userInput<128){
-          let i = 0;
-            while(i<userInput)
+        if (userInput>=8 && userInput<=128)
+        {
+          if(confirm("would you like to add uppercase letters?"))
+          {
+            if(confirm("would you like to add lowercase letters?"))
             {
-                let repeatValue = printValues[Math.floor(Math.random() * printValues.length)];
-                storeValue += repeatValue();
-                i=i+1;
+              if(confirm("would you like to add numeric characters?"))
+              {
+                let i = 0;
+                while(i<userInput)
+                {
+                    let repeatValue = printValues[Math.floor(Math.random() * printValues.length)];
+                    storeValue += repeatValue();
+                    i=i+1;
+                }
+              }  
             }
+          }
+          else if(confirm("would you like to add lower characters?"))
+          {
+            if(confirm("would you like to add numeric characters?"))
+            {
+              let i = 0;
+              while(i<userInput)
+              {
+                  let repeatValue = noUppercase[Math.floor(Math.random() * noUppercase.length)];
+                  storeValue += repeatValue();
+                  i=i+1;
+              }
+            }
+          }
+          else if(confirm("would you like to add numeric characters?"))
+          {
+              let i = 0;
+              while(i<userInput)
+              {
+                  let repeatValue = noLowercase[Math.floor(Math.random() * noLowercase.length)];
+                  storeValue += repeatValue();
+                  i=i+1;
+              }
+          }      
         }
         
-  
   return storeValue;
 }
 
